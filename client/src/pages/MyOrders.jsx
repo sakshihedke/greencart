93258520e74d0d177a965bb6879777cd044d57c7
Sakshi Hedke <sakshihedke6@gmail.com>
2025-06-04 22:51:26 +0530

    Describe the changes you made

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext';
-import { dummyOrders } from '../assets/assets';
-import { set } from 'mongoose';
 const MyOrders = () => {
     const [myOrders, setMyOrders] = useState([]);
     const {currency, axios, user} = useAppContext();
