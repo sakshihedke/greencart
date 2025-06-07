3a1eba540f4f5374573131af3fd01cb12bd1079b
Sakshi Hedke <sakshihedke6@gmail.com>
2025-06-07 09:53:52 +0530

    Fix: user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,11 @@
-// import mongoose from "mongoose";
+// User.js
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-
-  cartItems: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-      quantity: { type: Number, default: 1 },
-    }
-  ],
+  cartItems: { type: Schema.Types.Mixed, default: {} },
 });
 
 const User = mongoose.models.user || mongoose.model("user", userSchema);
