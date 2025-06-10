1f21a2a9d3c38b8e9112fb218e4418aeb42350fc
Sakshi Hedke <sakshihedke6@gmail.com>
2025-06-10 20:57:58 +0530

    Fix : auto logout user on homepage visit by calling logout API

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,20 @@
-import React, { useEffect } from 'react';
-import MainBanner from '../components/MainBanner';
-import Categories from '../components/Categories';
-import BestSeller from '../components/BestSeller';
-import BottomBanner from '../components/BottomBanner';
-import NewsLetter from '../components/NewsLetter';
-import { useAppContext } from '../context/AppContext';
-import axios from 'axios';
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
+import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
-  const { setUser } = useAppContext();
-
-  useEffect(() => {
-    const autoLogout = async () => {
-      try {
-        await axios.post('/api/user/logout', {}, { withCredentials: true });
-        setUser(null);
-        console.log('✅ User logged out on homepage visit');
-      } catch (error) {
-        console.error('❌ Auto logout error:', error);
-      }
-    };
-
-    autoLogout();
-  }, []);
-
   return (
-    <div className="mt-10">
-      <MainBanner />
-      <Categories />
-      <BestSeller />
-      <BottomBanner />
-      <NewsLetter />
+    <div className='mt-10'>
+        <MainBanner />
+        <Categories />
+        <BestSeller />
+        <BottomBanner />
+        <NewsLetter />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
